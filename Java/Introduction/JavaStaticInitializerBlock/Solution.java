cbb3baecd5dbc656bcff436d3e89f86cb171e2bc
h-xDF <-->
2017-04-11 14:29:46 +0300

    solved "int to string" and "static block"

--- Java/Introduction/JavaStaticInitializerBlock/Solution.java
+++ Java/Introduction/JavaStaticInitializerBlock/Solution.java
@@ -5,8 +5,7 @@ import java.util.*;
 public class Solution{
 
     static Boolean flag = true;
-    static int B;
-    static int H;
+    static int B, H;
 
     static  {
         Scanner in = new Scanner(System.in);
@@ -22,6 +21,17 @@ public class Solution{
         }else {
             flag = true;
         }
+
+        /**
+         * try {
+         if (B <= 0 || H <= 0)
+         throw new Exception("Breadth and height must be positive");
+         } catch (Exception e) {
+         System.out.println(e);
+         System.exit(0);
+         }
+         *
+         * */
     }
 
     public static void main(String[] args){
