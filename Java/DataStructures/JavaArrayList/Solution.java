4082196b0b1d88fe6c8fd75c591573fc8660b48b
h-xDF <raralux@yandex.ru>
2017-04-14 17:27:22 +0300

    +

--- Java/DataStructures/JavaArrayList/Solution.java
+++ Java/DataStructures/JavaArrayList/Solution.java
@@ -12,18 +12,48 @@ public class Solution {
 
         Scanner sc = new Scanner(System.in);
 
-        int N = sc.nextInt();
+        int N = Integer.parseInt(sc.nextLine()); // Count line
 
-        ArrayList<ArrayList<Integer>> arr = new ArrayList<ArrayList<Integer>>();
+        ArrayList<List<Integer>> arr = new ArrayList<List<Integer>>();
         for(int i = 0; i < N; i++) {
 
+            int count = sc.nextInt();
             arr.add(new ArrayList<Integer>());
-            int row = sc.nextInt();
-            for ( int j = 0; j < row; i++) {
-                arr.get(i).add(j, sc.nextInt());
+
+            for(int j = 0; j < count; j++) {
+
+                arr.get(i).add(sc.nextInt());
+            }
+            /*
+            arr.add(new ArrayList<Integer>());
+            //sc.next();
+            String row = sc.nextLine();
+            //int count = sc.nextInt();
+            Scanner rowSc = new Scanner(row);
+            while(rowSc.hasNextInt()) {
+                arr.get(i).add(rowSc.nextInt());
+            }*/
+        }
+
+        System.out.println(arr);
+
+        int M = sc.nextInt();// count check
+
+        // coordinate 'check'
+        int x,y;
+
+        for (int i = 0; i < M; i ++) {
+            x = sc.nextInt();
+            y = sc.nextInt();
+
+            try {
+                System.out.println(arr.get(x - 1).get(y - 1));
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Error");
             }
         }
 
-        System.out.println(arr.get(0).get(0));
+
+
     }
 }
