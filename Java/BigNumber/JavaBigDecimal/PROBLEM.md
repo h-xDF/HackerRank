8f8f0a1c94dd26aa540528de7b08649adf6690a5
h-xDF <raralux@yandex.ru>
2017-07-10 16:30:49 +0300

    add BigDecimal solved

---
+++ Java/BigNumber/JavaBigDecimal/PROBLEM.md
@@ -0,0 +1,32 @@
+The first line consists of a single integer, denoting the number of integer strings.
+Each line of the subsequent lines contains a real number.
+
+Sort real number strings in descending order
+
+## sample input
+``` 
+9
+-100
+50
+0
+56.6
+90
+0.12
+.12
+02.34
+000.000
+```
+
+
+## sample output
+``` 
+90
+56.6
+50
+02.34
+0.12
+.12
+0
+000.000
+-100
+```
---
+++ Java/BigNumber/JavaBigDecimal/Solution.java
@@ -0,0 +1,34 @@
+package BigNumber.JavaBigDecimal;
+
+import java.math.BigDecimal;
+import java.util.*;
+
+class Solution{
+
+    public static void main(String []args){
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] s = new String[n+2];
+
+        for (int i = 0; i < n; i++) {
+            s[i] = sc.next();
+        }
+        sc.close();
+
+        Comparator<String> decimalComparator = new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                BigDecimal a = new BigDecimal(o1);
+                BigDecimal b = new BigDecimal(o2);
+                return b.compareTo(a); //descending
+            }
+        };
+
+        Arrays.sort(s, 0, n, decimalComparator);
+
+        for (int i = 0; i < n; i++) {
+            System.out.println(s[i]);
+        }
+    }
+}
